8d781b6fcc5acff3534699fac2f7fca5b03b9964
Franklin Boampong <fboamps@gmail.com>
2020-06-07 22:46:50 -0400

    Added js file and added another css for styling

--- portfolio/src/main/webapp/script.js
+++ portfolio/src/main/webapp/script.js
@@ -1,28 +1,119 @@
-// Copyright 2019 Google LLC
-//
-// Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
-//
-//     https://www.apache.org/licenses/LICENSE-2.0
-//
-// Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
-// limitations under the License.
+jQuery(document).ready(() => {
+  // Logo animation
+  const $logo = $("#logo");
+  if (location.href.indexOf("#") !== -1) {
+    $logo.show();
+  }
 
-/**
- * Adds a random greeting to the page.
- */
-function addRandomGreeting() {
-  const greetings =
-      ['Hello world!', '¡Hola Mundo!', '你好，世界！', 'Bonjour le monde!'];
+  $(".menu .tabs a").click(() => {
+    $logo.fadeIn("slow");
+  });
 
-  // Pick a random greeting.
-  const greeting = greetings[Math.floor(Math.random() * greetings.length)];
+  $(".tab-profile").click(() => {
+    $logo.fadeOut("slow");
+  });
 
-  // Add it to the page.
-  const greetingContainer = document.getElementById('greeting-container');
-  greetingContainer.innerText = greeting;
-}
+  // Color manipulation
+  $("#yellow-color").click(function(e) {
+    $(".main-wrapper-resume").attr("id", "yellow");
+  });
+
+  $("#red-color").click(function(e) {
+    $(".main-wrapper-resume").attr("id", "red");
+  });
+
+  $("#blue-color").click(function(e) {
+    $(".main-wrapper-resume").attr("id", "blue");
+  });
+
+  $("#green-color").click(function(e) {
+    $(".main-wrapper-resume").attr("id", "green");
+  });
+
+  $("#setting-icon").click(function(e) {
+    $(".color-box").toggleClass("main");
+  });
+
+  // Portifolio items filtering
+  const $catsfilter = $(".cats-filter");
+  $catsfilter.find("a").click(function() {
+    $(this)
+      .parent()
+      .parent()
+      .find("a")
+      .removeClass("current");
+    $(this).addClass("current");
+  });
+
+  const $plist = $("#portifolio-list");
+  const $pfilter = $("#portifolio-filter");
+
+  $plist.isotope({
+    filter: "*",
+    layoutMode: "masonry",
+    animationOptions: {
+      duration: 750,
+      easing: "linear"
+    }
+  });
+
+  $pfilter.find("a").click(function() {
+    const selector = $(this).attr("data-filter");
+    $plist.isotope({
+      filter: selector,
+      layoutMode: "masonry",
+      animationOptions: {
+        duration: 750,
+        easing: "linear",
+        queue: false
+      }
+    });
+
+    return false;
+  });
+
+  // Profile image animation
+  $(".photo-inner ul").carouFredSel({
+    direction: "left",
+    circular: true,
+    auto: true,
+    scroll: {
+      items: 1,
+      fx: "crossfade",
+      duration: 1500,
+      wipe: true
+    },
+    swipe: {
+      onTouch: true
+    },
+    items: {
+      width: 153
+    }
+  });
+
+  // Menu nav bar manipulation
+  const $content = $("#content");
+
+  $content.easytabs({
+    animate: true,
+    updateHash: false,
+    transitionIn: "slideDown",
+    transitionOut: "slideUp",
+    animationSpeed: 600,
+    tabs: ".tmenu",
+    tabActiveClass: "active"
+  });
+
+  $content.find(".tabs li a").hover(
+    function() {
+      $(this)
+        .stop()
+        .animate({ marginTop: "-7px" }, 200);
+    },
+    function() {
+      $(this)
+        .stop()
+        .animate({ marginTop: "0px" }, 300);
+    }
+  );
+});
